9d41355fcd836b76fd69317f9e737f9cda6c747a
durianchipz <30475367+durianchipz@users.noreply.github.com>
2017-07-27 00:41:14 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -1,5 +1,5 @@
 <?php
-$access_token = 'access_token='&'kAFEeH9a6hpr8nbX9DKJXVdcmYM/t1MnMY5/oondsuIZr1RVPTqkfistooX+urwxl03M7eISaakYbsDFah84YYu+B2bxA3stccnqFjy8L0wYNozGQMWvpPJpEemztKySWxPC/OfRZJ79J3RfHgUZrAdB04t89/1O/w1cDnyilFU=';
+$access_token = 'access_token=kAFEeH9a6hpr8nbX9DKJXVdcmYM/t1MnMY5/oondsuIZr1RVPTqkfistooX+urwxl03M7eISaakYbsDFah84YYu+B2bxA3stccnqFjy8L0wYNozGQMWvpPJpEemztKySWxPC/OfRZJ79J3RfHgUZrAdB04t89/1O/w1cDnyilFU=';
 
 $url = 'https://api.line.me/v2/oauth/verify';
 
@@ -8,7 +8,7 @@ $url = 'https://api.line.me/v2/oauth/verify';
 $ch = curl_init($url);
 curl_setopt($ch, CURLOPT_POST, 1);
 curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/x-www-form-urlencoded'));
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+//curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 //curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
 //curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
 curl_setopt($ch, CURLOPT_POSTFIELDS,$access_token);
