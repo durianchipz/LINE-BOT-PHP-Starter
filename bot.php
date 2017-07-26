8d91d09241d27804ef54837b91bfa4d062b70d2f
durianchipz <30475367+durianchipz@users.noreply.github.com>
2017-07-27 00:23:11 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -1,2 +1,15 @@
 <?php
-echo "I am a bot";
+$access_token = 'kAFEeH9a6hpr8nbX9DKJXVdcmYM/t1MnMY5/oondsuIZr1RVPTqkfistooX+urwxl03M7eISaakYbsDFah84YYu+B2bxA3stccnqFjy8L0wYNozGQMWvpPJpEemztKySWxPC/OfRZJ79J3RfHgUZrAdB04t89/1O/w1cDnyilFU=';
+
+$url = 'https://api.line.me/v2/oauth/verify';
+
+$headers = array('Authorization: Bearer ' . $access_token);
+
+$ch = curl_init($url);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+$result = curl_exec($ch);
+curl_close($ch);
+
+echo $result;
